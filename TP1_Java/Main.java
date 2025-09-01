f5b5a300ac2ed3cdf86c183dca7b75fe26218733
Nicolas <nicolasarrastia@gmail.com>
2025-08-31 23:27:01 -0300

    move tp 1

---
+++ TP1_Java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        int suma = 0;
+
+
+
+        for (int i = 9; i >= 1; i -=2) {
+
+            suma += i;
+
+        }
+
+        System.out.println("Resultado: " + suma);
+
+    }
+
+}
+
